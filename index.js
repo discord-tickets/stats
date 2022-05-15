569f5090d88b63962cb44f4f427b88c4d53a61ce
Isaac <git@eartharoid.me>
2022-05-15 18:30:59 +0100

    Improvements

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-import { Router } from 'itty-router';
-import { createClient } from '@supabase/supabase-js';
 import joi from 'joi';
 import md5 from 'md5';
+import { createClient } from '@supabase/supabase-js';
+import { Router } from 'itty-router';
 
-const router = Router();
+const schema = joi.object({
+	activated_users: joi.number().integer(),
+	arch: joi.any().valid('arm', 'arm64', 'ia32', 'mips', 'mipsel', 'ppc', 'ppc64', 's390', 's390x', 'x64'),
+	avg_response_time: joi.number(), // in minutes
+	categories: joi.number().integer(),
+	guilds: joi.number().integer(),
+	id: joi.string().required(),
+	members: joi.number().integer(),
+	messages: joi.number().integer(),
+	node: joi.string().pattern(/^v\d+\.\d+\.\d+$/),
+	os: joi.any().valid('aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos', 'win32'),
+	tags: joi.number().integer(),
+	tickets: joi.number().integer().required(),
+	version: joi.string().pattern(/^\d+\.\d+\.\d+$/),
+}).rename('client', 'id');
 // eslint-disable-next-line no-undef
 const supabase = createClient(SUPABASE_URL, SUPABASE_SECRET);
 
-router.get('/');
-
-router.post('/client', async request => {
+async function updateStats(request, compatMode = false) {
 	const body = await request.json();
-	const schema = joi.object({
-		id: joi.string().required(),
-		tickets: joi.number().integer().required(),
-	});
 	const {
 		error: validationError,
 		value,
@@ -22,76 +30,28 @@ router.post('/client', async request => {
 
 	if (validationError) return new Response(validationError, { status: 400 });
 
-	value.id = md5(value.id);
-	value.last_seen = new Date();
+	if (compatMode) value.id = md5(value.id);
 
 	const { error } = await supabase.from('stats:clients').upsert(value, { returning: 'minimal' });
 
 	if (error) return new Response(error, { status: 500 });
 	else return new Response('OK', { status: 200 });
-});
+}
+
+const router = Router();
+
+router.get('/');
+
+router.post('/client', async request => await updateStats(request, true));
 
 router.get('/api/v3/current');
 
 router.get('/api/v3/history');
 
-router.post('/api/v3/houston', async request => {
-	const body = await request.json();
-	const schema = joi.object({
-		activated_users: joi.number().integer(),
-		arch: joi.string(),
-		avg_response_time: joi.number(), // in minutes
-		categories: joi.number().integer(),
-		guilds: joi.number().integer().required(),
-		id: joi.string().required(), // pre-hashed (md5)
-		members: joi.number().integer().required(),
-		messages: joi.number().integer(),
-		node: joi.string(),
-		os: joi.string(),
-		tags: joi.number().integer(),
-		tickets: joi.number().integer().required(),
-		version: joi.string(),
-	});
-	const {
-		error: validationError,
-		value,
-	} = schema.validate(body);
+router.post('/api/v3/houston', async request => await updateStats(request, false));
 
-	if (validationError) return new Response(validationError, { status: 400 });
+router.post('/v2', async request => await updateStats(request, true));
 
-	value.last_seen = new Date();
-
-	const { error } = await supabase.from('stats:clients').upsert(value, { returning: 'minimal' });
-
-	if (error) return new Response(error, { status: 500 });
-	else return new Response('OK', { status: 200 });
-});
-
-router.post('/v2', async request => {
-	const body = await request.json();
-	const schema = joi.object({
-		client: joi.string().required(),
-		guilds: joi.number().integer().required(),
-		members: joi.number().integer().required(),
-		tickets: joi.number().integer().required(),
-		version: joi.string(),
-	});
-	const {
-		error: validationError,
-		value,
-	} = schema.validate(body);
-
-	if (validationError) return new Response(validationError, { status: 400 });
-
-	value.id = md5(value.client);
-	delete value.client;
-	value.last_seen = new Date();
-
-	const { error } = await supabase.from('stats:clients').upsert(value, { returning: 'minimal' });
-
-	if (error) return new Response(error, { status: 500 });
-	else return new Response('OK', { status: 200 });
-});
 
 router.all('*', () => new Response('Not Found', { status: 404 }));
 
