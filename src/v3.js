84427613abe75c55e74af90e7b5ccef6fd4436fa
Isaac <git@eartharoid.me>
2023-07-13 02:33:19 +0100

    Set `first_seen`

--- src/v3.js
+++ src/v3.js
@@ -12,23 +12,25 @@ import {
 } from './utils';
 
 const semver = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
-const schema = joi.object({
-	activated_users: joi.number().integer(),
-	arch: joi.any().valid('arm', 'arm64', 'ia32', 'mips', 'mipsel', 'ppc', 'ppc64', 's390', 's390x', 'x64'),
-	avg_resolution_time: joi.number(), // in minutes
-	avg_response_time: joi.number(), // in minutes
-	categories: joi.number().integer(),
-	database: joi.any().valid('mysql', 'postgresql', 'sqlite'),
-	guilds: joi.number().integer(),
-	id: joi.string().required(),
-	members: joi.number().integer(),
-	messages: joi.number().integer(),
-	node: joi.string().pattern(/^v\d+\.\d+\.\d+$/),
-	os: joi.any().valid('aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos', 'win32'),
-	tags: joi.number().integer(),
-	tickets: joi.number().integer().required(),
-	version: joi.string().pattern(semver),
-}).rename('client', 'id');
+const schema = joi
+	.object({
+		activated_users: joi.number().integer(),
+		arch: joi.any().valid('arm', 'arm64', 'ia32', 'mips', 'mipsel', 'ppc', 'ppc64', 's390', 's390x', 'x64'),
+		avg_resolution_time: joi.number(), // in minutes
+		avg_response_time: joi.number(), // in minutes
+		categories: joi.number().integer(),
+		database: joi.any().valid('mysql', 'postgresql', 'sqlite'),
+		guilds: joi.number().integer(),
+		id: joi.string().required(),
+		members: joi.number().integer(),
+		messages: joi.number().integer(),
+		node: joi.string().pattern(/^v\d+\.\d+\.\d+$/),
+		os: joi.any().valid('aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos', 'win32'),
+		tags: joi.number().integer(),
+		tickets: joi.number().integer().required(),
+		version: joi.string().pattern(semver),
+	})
+	.rename('client', 'id');
 
 export const updateClient = async (req, compatMode = false) => {
 	const body = await req.json();
@@ -45,6 +47,7 @@ export const updateClient = async (req, compatMode = false) => {
 	return await db(req).collection('clients').updateOne(
 		{ _id: id },
 		{ $set: value },
+		{ $setOnInsert: { first_seen: new Date() } },
 		{ upsert: true },
 	);
 };
